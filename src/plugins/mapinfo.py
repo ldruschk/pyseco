23c082a62d1e3bd3f0552bb32d87e13c429da20f
Lucas <mikotosc@gmail.com>
2015-09-16 12:53:51 +0200

    [mapinfo] at the end of the map the info for the next map is shown

--- src/plugins/mapinfo.py
+++ src/plugins/mapinfo.py
@@ -10,14 +10,15 @@ class mapinfo(pyseco_plugin):
         <label posn='%f %f %f' sizen='%f %f' textsize='%f' text='%s'/>
         <label posn='%f %f %f' sizen='%f %f' textsize='%f' text='%s'/>
         <label posn='%f %f %f' sizen='%f %f' textsize='%f' text='%s'/>
+        %s
     </frame>
 </manialink>"""
+    next_str = "<label posn='%f %f %f' sizen='%f %f' textsize='%f' text='%s'/>"
 
     def __init__(self, pyseco):
         pyseco_plugin.__init__(self, pyseco)
         self.register_callback("TrackMania.BeginChallenge")
-        self.register_callback("TrackMania.EndChallenge")
-        self.register_callback("TrackMania.StatusChanged")
+        self.register_callback("TrackMania.EndRound")
         self.register_callback("TrackMania.PlayerConnect")
 
         self.initialize()
@@ -25,28 +26,43 @@ class mapinfo(pyseco_plugin):
     def initialize(self):
         response = self.pyseco.query((), "GetCurrentMapInfo")
         self.show_current_info(response[0][0])
+        self.ingame = True
 
     def process_callback(self, value):
         if value[1] == "TrackMania.BeginChallenge":
+            self.ingame = True
             response = self.pyseco.query((), "GetCurrentMapInfo")
             self.show_current_info(response[0][0])
         elif value[1] == "TrackMania.PlayerConnect":
-            response = self.pyseco.query((), "GetCurrentMapInfo")
-            self.show_current_info(response[0][0], login=value[0][0])
+            if self.ingame:
+                response = self.pyseco.query((), "GetCurrentMapInfo")
+                self.show_current_info(response[0][0], login=value[0][0])
+            else:
+                response = self.pyseco.query((), "GetNextMapInfo")
+                self.show_current_info(response[0][0], login=value[0][0], next_=True)
+        elif value[1] == "TrackMania.EndRound":
+            self.ingame = False
+            response = self.pyseco.query((), "GetNextMapInfo")
+            self.show_current_info(response[0][0], next_=True)
 
 
-    def show_current_info(self, info, login=None):
+    def show_current_info(self, info, login=None, next_=False):
         name = info["Name"].replace("'","&apos;")
         author = info["Author"].replace("'","&apos;")
         time = info["AuthorTime"]
         timestr = "%d.%03d" % (int(time/1000), time%1000)
+
+        next_str = ""
+        if next_:
+            next_str = self.next_str % (-6, -1, 0, 5, 1, 1, "$sNext Map:")
 
         xml = self.info_manialink % ("mapinfo",
             48.75, 47.75, 0,
             15, 7, "BgsPlayerCard", "BgCard",
             1, -1, 0, 13, 2, 2, name,
             1, -3.25, 0, 13, 1, 1, author,
-            1, -5, 0, 13, 1, 1, timestr
+            1, -5, 0, 13, 1, 1, timestr,
+            next_str
         )
         if login is None:
             response = self.pyseco.query((xml, 0, False), "SendDisplayManialinkPage")

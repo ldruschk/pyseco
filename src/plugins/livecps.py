67793bf4fa68474f34506c779f0191fd5165446a
Lucas <mikotosc@gmail.com>
2015-09-16 12:46:48 +0200

    [livecps] manialinks now also shown to new connections

--- src/plugins/livecps.py
+++ src/plugins/livecps.py
@@ -28,18 +28,35 @@ class livecps(pyseco_plugin):
     def __init__(self, pyseco):
         pyseco_plugin.__init__(self, pyseco)
         self.initialize()
-        self.pyseco.add_callback_listener("TrackMania.PlayerCheckpoint", self)
-        self.pyseco.add_callback_listener("TrackMania.BeginRace", self)
+        self.register_callback("TrackMania.PlayerCheckpoint")
+        self.register_callback("TrackMania.BeginRace")
+        self.register_callback("TrackMania.PlayerConnect")
+        self.register_callback("TrackMania.EndRound")
 
     def initialize(self):
         value = self.pyseco.query((), "GetCurrentMapInfo")
         self.new_map(value[0])
+        self.ingame = True
 
     def process_callback(self, value):
         if value[1] == "TrackMania.PlayerCheckpoint":
             self.process_checkpoint(value[0])
-        if value[1] == "TrackMania.BeginRace":
+        elif value[1] == "TrackMania.BeginRace":
             self.new_map(value[0])
+            self.ingame = True
+        elif value[1] == "TrackMania.PlayerConnect":
+            if self.ingame:
+                self.new_player(value[0][0])
+        elif value[1] == "TrackMania.EndRound":
+            self.ingame = False
+
+    def new_player(self, login):
+        for cp, val in self.checkpoint_recs.items():
+            if cp == self.nb_checkpoints - 1:
+                continue
+            self.update_checkpoint_manialink(cp, val["time"],
+                self.pyseco.get_player(val["player"]).get_nick_name(),
+                login=login)
 
     def process_checkpoint(self, value):
         player = value[1]
@@ -53,13 +70,16 @@ class livecps(pyseco_plugin):
             self.update_checkpoint_manialink(
                 cp, time, self.pyseco.get_player(player).get_nick_name())
 
-    def update_checkpoint_manialink(self, num_cp, time, name):
+    def update_checkpoint_manialink(self, num_cp, time, name, login=None):
         row = int(num_cp / 6)
         column = num_cp % 6
         id_ = "livecps%d" % num_cp
         self.pyseco.add_manialink(id_, ingame=True)
         xml = self.manialink_str % (id_, -48.5 + column*16, 45 - row*4, 0, 15, 3, "BgsPlayerCard", "BgCard", 0.75, -0.75, 0, 10.5, 1.5, 1.5, "$fffCP%d: %s" % (num_cp+1, name.replace("'","&apos;")), 11.5, -0.75, 0, 2.75, 1.5, 1.5, "$0$fff| %d.%03d" % (int(time/1000), time % 1000))
-        self.pyseco.query((xml, 0, False), "SendDisplayManialinkPage")
+        if login is None:
+            self.pyseco.query((xml, 0, False), "SendDisplayManialinkPage")
+        else:
+            self.pyseco.query((login, xml, 0, False), "SendDisplayManialinkPageToLogin")
 
     def new_map(self, value):
         self.nb_checkpoints = value[0]["NbCheckpoints"]
